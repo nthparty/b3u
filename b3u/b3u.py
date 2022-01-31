ed2c28081caf2e6c57f7e59af77bb83926a43529
tsbraun1891 <tsbraun1891@gmail.com>
2022-01-31 16:24:53 -0500

    Objectify b3u library and add pytest testing

--- b3u/b3u.py
+++ b3u/b3u.py
@@ -2,257 +2,237 @@
 Boto3 URI utility library that supports extraction of
 Boto3 configuration data from AWS resource URIs.
 """
+
 from __future__ import annotations
 import doctest
+from tkinter.messagebox import NO
 from urllib.parse import urlparse, parse_qs, quote, unquote, ParseResult
 
-def credentials(uri: str) -> dict:
-    """
-    Extract configuration data (only credentials) from a URI string.
+class b3u:
+    def __init__(self, uri: str):
 
-    >>> credentials('s3://abc:xyz@bucket/object.data')
-    {'aws_access_key_id': 'abc', 'aws_secret_access_key': 'xyz'}
+        result = self._make_url_safe(uri)
 
-    The format ``abc:xyz:123`` can be used to specify a session
-    token (which is the third component, ``123``, in this example)
-    as part of a URI.
+        self.Bucket = None
+        self.Key = None
+        self.Name = None
 
-    >>> cs = credentials('s3://abc:xyz:123@bucket/object.data')
-    >>> for (k, v) in sorted(cs.items()):
-    ...     print(k, v)
-    aws_access_key_id abc
-    aws_secret_access_key xyz
-    aws_session_token 123
+        if result.scheme == 's3':
+            if result.hostname is not None and result.hostname != '':
+                self.Bucket = result.hostname
+            if result.path is not None and result.path != '':
+                self.Key =  result.path.lstrip('/')
+        elif result.scheme == 'ssm':
+            if result.path is not None and result.path != '':
+                self.Name = result.path
 
-    Credential information can contain the ``/`` character, accommodating
-    more complex scenarios.
+        params = {}
+        result = self._make_url_safe(uri)
 
-    >>> cs = credentials('s3://abc:/abcdef/ghijklmnopqrstuvwxyz/1234567890/@bucket/object.data')
-    >>> for (k, v) in sorted(cs.items()):
-    ...     print(k, v)
-    aws_access_key_id abc
-    aws_secret_access_key /abcdef/ghijklmnopqrstuvwxyz/1234567890/
-
-    >>> cs = credentials('s3://abc:/abcdef/ghijklmnopqrstuvwxyz/1234567890/:123@bucket/object.data')
-    >>> for (k, v) in sorted(cs.items()):
-    ...     print(k, v)
-    aws_access_key_id abc
-    aws_secret_access_key /abcdef/ghijklmnopqrstuvwxyz/1234567890/
-    aws_session_token 123
-    """
-    params = {}
-    result = _make_url_safe(uri)
-
-    if result.username is not None and result.username != '':
-        params['aws_access_key_id'] = result.username
-
-    if result.password is not None and result.password != '':
-        if not ':' in result.password:
-            params['aws_secret_access_key'] = unquote(result.password)
+        if result.username is not None and result.username != '':
+            self.aws_access_key_id = result.username
         else:
-            (secret, token) = result.password.split(':')
-            params['aws_secret_access_key'] = unquote(secret)
-            params['aws_session_token'] = token
+            self.aws_access_key_id = None
 
-    return params
+        if result.password is not None and result.password != '':
+            if not ':' in result.password:
+                self.aws_secret_access_key = unquote(result.password)
+                self.aws_session_token = None
+            else:
+                (secret, token) = result.password.split(':')
+                self.aws_secret_access_key = unquote(secret)
+                self.aws_session_token = token
+        else:
+            self.aws_secret_access_key = None
+            self.aws_session_token = None
 
-def configuration(uri: str, safe: bool = True) -> dict:
-    """
-    Extract configuration data (both credentials and non-credentials)
-    from a URI string.
+        result = parse_qs(self._make_url_safe(uri).query)
 
-    >>> configuration('s3://abc:xyz@bucket/object.data')
-    {'aws_access_key_id': 'abc', 'aws_secret_access_key': 'xyz'}
-
-    The format ``abc:xyz:123`` can be used to specify a session
-    token (which is ``123`` in this case).
-
-    >>> cs = configuration('s3://abc:xyz:123@bucket/object.data')
-    >>> for (k, v) in sorted(cs.items()):
-    ...     print(k, v)
-    aws_access_key_id abc
-    aws_secret_access_key xyz
-    aws_session_token 123
-
-    The value for the parameter ``region_name`` can be specified in the
-    manner illustrated below.
-
-    >>> cs = configuration('s3://abc:xyz@bucket/object.data?region_name=us-east-1')
-    >>> for (k, v) in sorted(cs.items()):
-    ...     print(k, v)
-    aws_access_key_id abc
-    aws_secret_access_key xyz
-    region_name us-east-1
-
-    >>> cs = configuration('s3://bucket/object.data?region_name=us-east-1')
-    >>> for (k, v) in sorted(cs.items()):
-    ...     print(k, v)
-    region_name us-east-1
-
-    Other named parameters in the URI are ignored by default, but can be
-    included by setting the function parameter ``safe`` to ``False``.
-
-    >>> cs = configuration('s3://bucket/object.data?other_param=other_value')
-    >>> for (k, v) in sorted(cs.items()):
-    ...     print(k, v)
-
-    >>> cs = configuration('s3://bucket/object.data?other_param=other_value', False)
-    >>> for (k, v) in sorted(cs.items()):
-    ...     print(k, v)
-    other_param other_value
-
-    >>> cs = configuration('s3://bucket/object.data?other_param=other:value', False)
-    >>> for (k, v) in sorted(cs.items()):
-    ...     print(k, v)
-    other_param other:value
-    """
-    params = credentials(uri)
-    result = parse_qs(_make_url_safe(uri).query)
-
-    for (key, values) in result.items():
-        if len(values) == 1:
-            if not safe or key in [
-                'aws_access_key_id', 'aws_secret_access_key',
-                'aws_session_token', 'region_name'
-            ]:
+        for (key, values) in result.items():
+            if len(values) == 1:
                 params[key] = values[0]
 
-    return params
+        result = self._make_url_safe(uri)
+        self.service_name = result.scheme
 
-def for_client(uri: str, safe: bool = True) -> dict:
-    """
-    Extract parameters for a client constructor from a URI string.
+        # Extract remaining default/'safe' properties from params
+        # so that all that remains is custom values
+        self.region_name = params.pop('region_name', None)
+        self.api_version = params.pop('api_version', None)
+        self.endpoint_url = params.pop('endpoint_url', None)
+        self.verify = params.pop('verify', None)
+        self.config = params.pop('config', None)
 
-    >>> ps = for_client('s3://abc:xyz@bucket/object.data?region_name=us-east-1')
-    >>> for (k, v) in sorted(ps.items()):
-    ...     print(k, v)
-    aws_access_key_id abc
-    aws_secret_access_key xyz
-    region_name us-east-1
-    service_name s3
+        # Only values left in params should be custom user values
+        self.custom_values = params
 
-    By default, only the parameters ``service_name``, ``region_name``,
-    ``api_version``, ``endpoint_url``, ``verify``, ``aws_access_key_id``,
-    ``aws_secret_access_key``, ``aws_session_token``, and ``config`` are
-    extracted from the URI string.
+        # Extract remaining properties (custom values given by user)
+        # so that they can be accessed by foo.<custom_parameter_name>
+        self._extract_custom_properties(self.custom_values)
 
-    >>> ps = for_client('s3://abc:xyz@bucket/object.data?other_param=other_value')
-    >>> for (k, v) in sorted(ps.items()):
-    ...     print(k, v)
-    aws_access_key_id abc
-    aws_secret_access_key xyz
-    service_name s3
 
-    Other named parameters in the URI string are ignored by default, but can
-    be included by setting the function parameter ``safe`` to ``False``.
+    # Extract all custom values into properties
+    def _extract_custom_properties(self, params: dict):
+        for key in params.keys():
+            setattr(self, key, params[key])
 
-    >>> ps = for_client('s3://abc:xyz@bucket/object.data?other_param=other_value', False)
-    >>> for (k, v) in sorted(ps.items()):
-    ...     print(k, v)
-    aws_access_key_id abc
-    aws_secret_access_key xyz
-    other_param other_value
-    service_name s3
-    """
-    result = _make_url_safe(uri)
-    params = configuration(uri, False)
-    params['service_name'] = result.scheme
+    # Given a list of property names, creates a dictionary with structure property_name: value if value is not None
+    # If safe is false, includes all custom values as well
+    def _package_properties(self, property_list: list, safe: bool=True):
+        result = {}
 
-    # Keep only those parameters that correspond to named arguments
-    # of the target method.
-    if safe:
-        params = {
-            param: value
-            for (param, value) in params.items()
-            if param in [
-                'service_name', 'region_name', 'api_version', 'endpoint_url',
-                'verify', 'aws_access_key_id', 'aws_secret_access_key',
-                'aws_session_token', 'config'
-            ]
-        }
+        for key_val in property_list:
+            att_val = self.__getattribute__(key_val)
+            if att_val is not None:
+                result[key_val] = att_val
+        
+        if not safe:
+            result.update(self.custom_values)
 
-    return params
+        return result
 
-def for_resource(uri: str, safe: bool = True) -> dict:
-    """
-    Extract parameters for a resource constructor from a URI string.
-    This function is a synonym for the :obj:`for_client` function.
+    def credentials(self):
+        """
+        Extract configuration data (only credentials) from a URI string.
+        """
 
-    >>> ps = for_resource('s3://abc:xyz@bucket/object.data?region_name=us-east-1')
-    >>> for (k, v) in sorted(ps.items()):
-    ...     print(k, v)
-    aws_access_key_id abc
-    aws_secret_access_key xyz
-    region_name us-east-1
-    service_name s3
-    """
-    return for_client(uri, safe)
+        return self._package_properties(['aws_access_key_id', 'aws_secret_access_key', 'aws_session_token'])
+    
+    def configuration(self, safe: bool=True):
+        """
+        Extract configuration data (both credentials and non-credentials)
+        from a URI string. 
+        """
 
-def _make_url_safe(uri: str) -> ParseResult:
-    """
-    URL encode slashes in ``aws_secret_access_key`` to ensure compatibility
-    with ``urlparse``.
+        return self._package_properties(['aws_access_key_id', 'aws_secret_access_key', 'aws_session_token', 'region_name'], safe)
+    
+    def for_client(self, safe: bool=True):
+        """
+        Extract parameters for a client constructor from a URI string.
+        """
 
-    :param uri: AWS resource URI
-    :return: URL parsed URI with slahes in ``aws_secret_access_key`` encoded
-    """
-    parts = uri.split(':')
-    if len(parts) >= 3:
-        key_and_bucket = parts[2].split('@')
-        if len(key_and_bucket[0]) == 40:
-            key_and_bucket[0] = quote(key_and_bucket[0], safe='')
+        return self._package_properties(['service_name', 'region_name', 'api_version', 'endpoint_url', 'verify', 'aws_access_key_id', 'aws_secret_access_key', 'aws_session_token', 'config'], safe)
 
-        if len(parts) >= 4:
-            uri = ':'.join(parts[:2]) + ':' + ''.join(key_and_bucket) + ':' + ':'.join(parts[3:])
-        else:
-            uri = ':'.join(parts[:2]) + ':' + '@'.join(key_and_bucket)
-    return urlparse(uri)
+    def for_resource(self, safe: bool=True):
+        """
+        Extract parameters for a resource constructor from a URI string.
+        This function is a synonym for the :obj:`for_client` function.
+        """
 
-def for_get(uri: str) -> dict:
-    """
-    Extract resource names from a URI for supported AWS services.
-    Currently, only S3 and SSM are supported.
+        return self.for_client(safe)
 
-    >>> for_get('s3://abc:xyz@bucket/object.data')
-    {'Bucket': 'bucket', 'Key': 'object.data'}
-    >>> for_get('ssm://ABC:XYZ@/path/to/parameter?region_name=us-east-1')
-    {'Name': '/path/to/parameter'}
-    """
-    params = {}
-    result = _make_url_safe(uri)
+    def _make_url_safe(self, uri: str) -> ParseResult:
+        """
+        URL encode slashes in ``aws_secret_access_key`` to ensure compatibility
+        with ``urlparse``.
 
-    if result.scheme == 's3':
-        if result.hostname is not None and result.hostname != '':
-            params['Bucket'] = result.hostname
-        if result.path is not None and result.path != '':
-            params['Key'] = result.path.lstrip('/')
-    elif result.scheme == 'ssm':
-        if result.path is not None and result.path != '':
-            params['Name'] = result.path
+        :param uri: AWS resource URI
+        :return: URL parsed URI with slahes in ``aws_secret_access_key`` encoded
+        """
+        parts = uri.split(':')
+        if len(parts) >= 3:
+            key_and_bucket = parts[2].split('@')
+            if len(key_and_bucket[0]) == 40:
+                key_and_bucket[0] = quote(key_and_bucket[0], safe='')
 
-    return params
+            if len(parts) >= 4:
+                uri = ':'.join(parts[:2]) + ':' + ''.join(key_and_bucket) + ':' + ':'.join(parts[3:])
+            else:
+                uri = ':'.join(parts[:2]) + ':' + '@'.join(key_and_bucket)
+        return urlparse(uri)
 
-def cred(uri: str) -> dict:
-    """
-    Concise synonym for the :obj:`credentials` function.
+    def for_get(self) -> dict:
+        """
+        Extract resource names from a URI for supported AWS services.
+        Currently, only S3 and SSM are supported.
+        """
+        if self.service_name == 's3':
+            return self._package_properties(['Bucket', 'Key'])
+        elif self.service_name == 'ssm':
+            return self._package_properties(['Name'])
 
-    >>> cs = cred('s3://abc:xyz:123@bucket/object.data')
-    >>> for (k, v) in sorted(cs.items()):
-    ...     print(k, v)
-    aws_access_key_id abc
-    aws_secret_access_key xyz
-    aws_session_token 123
-    """
-    return credentials(uri)
+        return {}
+       
 
-def conf(uri: str) -> dict:
-    """
-    Concise synonym for the :obj:`configuration` function.
+    def cred(self) -> dict:
+        """
+        Concise synonym for the :obj:`credentials` function.
+        """
+        return self.credentials()
 
-    >>> conf('s3://abc:xyz@bucket/object.data')
-    {'aws_access_key_id': 'abc', 'aws_secret_access_key': 'xyz'}
-    """
-    return configuration(uri)
+    def conf(self, safe: bool=True) -> dict:
+        """
+        Concise synonym for the :obj:`_configuration` function.
+        """
+        return self.configuration(safe)
+
+    def to_string(self) -> str:
+        """
+        Constructs a uri based off of whatever the current properties of this object are
+        """
+
+        new_uri = ""
+
+        if self.service_name is not None:
+            new_uri += self.service_name + "://"
+
+        contains_aws_info = False
+
+        if self.aws_access_key_id is not None:
+            new_uri += self.aws_access_key_id
+
+            contains_aws_info = True
+
+        if self.aws_secret_access_key is not None:
+            if contains_aws_info:
+                new_uri += ":"
+            else:
+                contains_aws_info = True
+            
+            new_uri += self.aws_secret_access_key
+            
+        if self.aws_session_token is not None:
+            if contains_aws_info:
+                new_uri += ":"
+            else:
+                contains_aws_info = True
+            
+            new_uri += self.aws_session_token
+
+        # Only include the @ if there was aws info included
+        if contains_aws_info:
+            new_uri += "@"
+        
+
+        if self.Bucket is not None:
+            new_uri += self.Bucket
+
+            # bucket must exist for key to exist
+            if self.Key is not None:
+                new_uri += "/" + self.Key
+        
+        elif self.Name is not None:
+            new_uri += self.Name
+
+        
+        # Add in all parameters including custom values
+        parameters = self._package_properties(['region_name', 'api_version', 'endpoint_url', 'verify', 'config'], False)
+
+        first_param = True
+        for key in parameters:
+            if first_param:
+                new_uri += "?"
+                first_param = False
+            else:
+                new_uri += "&"
+            
+            new_uri += key + "=" + parameters[key]
+
+        return new_uri
+
+        
+
 
 if __name__ == "__main__":
     doctest.testmod() # pragma: no cover
+
---
+++ b3u_test.py
@@ -0,0 +1,80 @@
+from pickle import FALSE
+from re import A
+from b3u.b3u_class import b3u
+
+
+def func():
+    print("hello")
+
+def test_credentials():
+    test_object = b3u('s3://abc:xyz@bucket/object.data')
+    assert test_object.cred() == {'aws_access_key_id': 'abc', 'aws_secret_access_key': 'xyz'}
+
+    test_object = b3u('s3://abc:xyz:123@bucket/object.data')
+    assert test_object.cred() == {'aws_access_key_id': 'abc', 'aws_secret_access_key': 'xyz', 'aws_session_token' : '123'}
+
+    test_object = b3u('s3://abc:/abcdef/ghijklmnopqrstuvwxyz/1234567890/@bucket/object.data')
+    assert test_object.cred() == {'aws_access_key_id': 'abc', 'aws_secret_access_key': '/abcdef/ghijklmnopqrstuvwxyz/1234567890/'}
+
+    test_object = b3u('s3://abc:/abcdef/ghijklmnopqrstuvwxyz/1234567890/:123@bucket/object.data')
+    assert test_object.cred() == {'aws_access_key_id': 'abc', 'aws_secret_access_key': '/abcdef/ghijklmnopqrstuvwxyz/1234567890/', 'aws_session_token' : '123'}
+
+
+    # Test if credentials can handle edited values
+    test_object = b3u('s3://abc:xyz:123@bucket/object.data')
+    test_object.aws_access_key_id = 'LMNO'
+    assert test_object.cred() == {'aws_access_key_id': 'LMNO', 'aws_secret_access_key': 'xyz', 'aws_session_token' : '123'}
+
+def test_configuration():
+    test_object = b3u('s3://abc:xyz@bucket/object.data')
+    assert test_object.conf() == {'aws_access_key_id': 'abc', 'aws_secret_access_key': 'xyz'}
+
+    test_object = b3u('s3://abc:xyz:123@bucket/object.data')
+    assert test_object.conf() == {'aws_access_key_id': 'abc', 'aws_secret_access_key': 'xyz', 'aws_session_token' : '123'}
+
+    test_object = b3u('s3://abc:xyz@bucket/object.data?region_name=us-east-1')
+    assert test_object.conf() == {'aws_access_key_id': 'abc', 'aws_secret_access_key': 'xyz', 'region_name' : 'us-east-1'}
+
+    test_object = b3u('s3://bucket/object.data?other_param=other_value')
+    assert test_object.conf(True) == {}
+    assert test_object.conf(False) == {'other_param' : 'other_value'}
+
+    test_object = b3u('s3://bucket/object.data?other_param=other:value')
+    assert test_object.conf(False) == {'other_param' : 'other:value'}
+
+    # Check that configuration can handle edited values
+    test_object = b3u('s3://abc:xyz@bucket/object.data?region_name=us-east-1')
+    test_object.aws_secret_access_key = '123'
+    test_object.aws_session_token = '456'
+    test_object.region_name = 'us-east-2'
+    assert test_object.conf() == {'aws_access_key_id': 'abc', 'aws_secret_access_key': '123', 'aws_session_token' : '456', 'region_name' : 'us-east-2'}
+
+
+def test_for_client():
+    test_object = b3u('s3://abc:xyz@bucket/object.data?region_name=us-east-1')
+    assert test_object.for_client() == {'aws_access_key_id' : 'abc', 'aws_secret_access_key' : 'xyz', 'region_name' : 'us-east-1', 'service_name' : 's3'}
+
+    """
+    By default, only the parameters ``service_name``, ``region_name``,
+    ``api_version``, ``endpoint_url``, ``verify``, ``aws_access_key_id``,
+    ``aws_secret_access_key``, ``aws_session_token``, and ``config`` are
+    extracted from the URI string.
+
+    Other named parameters in the URI string are ignored by default, but can
+    be included by setting the function parameter ``safe`` to ``False``.
+    """
+    test_object = b3u('s3://abc:xyz@bucket/object.data?other_param=other_value')
+    assert test_object.for_client() == {'aws_access_key_id' : 'abc', 'aws_secret_access_key' : 'xyz', 'service_name' : 's3'}
+    assert test_object.for_client(False) == {'aws_access_key_id' : 'abc', 'aws_secret_access_key' : 'xyz', 'service_name' : 's3', 'other_param' : 'other_value'}
+
+
+def test_for_resource(): 
+    test_object = b3u('s3://abc:xyz@bucket/object.data?region_name=us-east-1')
+    assert test_object.for_resource() == {'aws_access_key_id' : 'abc', 'aws_secret_access_key' : 'xyz', 'region_name' : 'us-east-1', 'service_name' : 's3'} 
+
+def test_for_get():
+    test_object = b3u('s3://abc:xyz@bucket/object.data')
+    assert test_object.for_get() == {'Bucket': 'bucket', 'Key': 'object.data'}
+
+    test_object = b3u('ssm://ABC:XYZ@/path/to/parameter?region_name=us-east-1')
+    assert test_object.for_get() == {'Name': '/path/to/parameter'}
